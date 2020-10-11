04d1cafe6e7264f4c399fbbea7ed5bb3367d750a
Benjamín Blanco <crbenjaminblanco@gmail.com>
2020-10-10 18:41:40 -0600

    Cambio a PDF!

--- src/app/proforma/proforma.component.ts
+++ src/app/proforma/proforma.component.ts
@@ -2,8 +2,24 @@ import { Component, OnInit, ElementRef, ViewChildren, QueryList, ViewChild} from
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import PizZipUtils from "pizzip/utils/index.js";
-import { saveAs } from "file-saver";
+import { saveAs, FileSaver } from "file-saver";
 import * as moment from 'moment';
+// ES6 Module
+import { initIva, convertDocxToPDFFromFile } from "iva-converter";
+
+
+//usado para generar el pdf
+export async function convertDocxToPDF(
+  file: any,
+  fileName: string
+): Promise<any> {
+  // GET YOUR API KEY AT https://app.iva-docs.com/auth/register
+  initIva("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjgyMzE0ZTljYTY5YjAwMjk5NmQxYjYiLCJjcmVhdGVkQXQiOjE2MDIzNjc4MjI2NTUsImlhdCI6MTYwMjM2NzgyMn0.2lMsFkDZdKKUauvWDqZnj0RQ-oam8sEASqv4RqEfTSI");
+  // This function returns a Promise that resolved a blob
+  const data = await convertDocxToPDFFromFile(file);
+  // File saver is a library that starts a download with data in blob format
+  await saveAs(data, fileName);
+}
 
 function loadFile(url, callback) {
   PizZipUtils.getBinaryContent(url, callback);
@@ -16,13 +32,14 @@ function loadFile(url, callback) {
 })
 export class ProformaComponent implements OnInit {
   numeroLineas:any=[''];
-  @ViewChild('numeroProforma') numeroProforma: any;  
+  @ViewChild('numeroProforma') numeroProforma: any; 
+  @ViewChild('descargarPDF') descargarPDF: any;  
   @ViewChildren('formulario') formularios: QueryList<any>;  
-    //console.log(this.cantidad0.nativeElement.value);
   arrayObjetos=[];
   IVA=0.13;
   subtotal;
   impVentas;
+  descargando=false;
   precioFinal;
   mensajeError;
   constructor() { }
@@ -37,7 +54,9 @@ export class ProformaComponent implements OnInit {
     if (this.numeroProforma.nativeElement.value!=''){
       if (flagPass)
       {
-        this.createWord(this.setAttributes()); //Solamente descarga el word}
+        this.descargando=true;
+        this.descargarPDF.nativeElement.innerHTML='Descargando';
+        this.createWord(this.setAttributes());
       }
       else
       {
@@ -116,7 +135,6 @@ export class ProformaComponent implements OnInit {
 
     /*Asigna los atributos del word*/
     setAttributes = () => {
-      console.log(this.arrayObjetos);
       let wordAttributes= {
         numeroProforma: this.numeroProforma.nativeElement.value,
         lineaTabla: this.arrayObjetos,
@@ -143,63 +161,76 @@ export class ProformaComponent implements OnInit {
 
   
     /*Inicio del script de word para crearlo segun la plantilla y descarga si viene desde el metodo ZIP*/
-    createWord = (myAttributes) => { 
+    createWord = function (myAttributes){ 
+     let promise=new Promise((resolve)=>{
       //url cambia de manera local
-      loadFile("././assets/templateProforma.docx", function (error, content) {
-        if (error) {
-          throw error;
-        }
-        var zip = new PizZip(content);
-        var doc = new Docxtemplater().loadZip(zip);
-        doc.setData(myAttributes);
-        try {
-          // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-          doc.render();
-        } catch (error) {
-          // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-          function replaceErrors(key, value) {
-            if (value instanceof Error) {
-              return Object.getOwnPropertyNames(value).reduce(function (
-                error,
-                key
-              ) {
-                error[key] = value[key];
-                return error;
-              },
-              {});
-            }
-            return value;
-          }
-          console.log(
-            JSON.stringify(
-              {
-                error: error,
-              },
-              replaceErrors
-            )
-          );
-  
-          if (error.properties && error.properties.errors instanceof Array) {
-            const errorMessages = error.properties.errors
-              .map(function (error) {
-                return error.properties.explanation;
-              })
-              .join("\n");
-            console.log("errorMessages", errorMessages);
-            // errorMessages is a humanly readable message looking like this :
-            // 'The tag beginning with "foobar" is unopened'
-          }
-          throw error;
-        }
-        var out = doc.getZip().generate({
-          type: "blob",
-          mimeType:
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        }); //Output the document using Data-URI
-          saveAs(out, "Proforma_"+myAttributes.numeroProforma+".docx"); //Descarga el archivo
-      
-      });
-    };
-    /*Fin del script de word */
+      loadFile("././assets/templateProforma.docx",(error,content)=>{
+        this.wordMainFunction(error,content,myAttributes);
+        resolve();
+     });
+    });
+      return promise;
+    //.descargando=false;
+  }
 
+       wordMainFunction =  function (error,content,myAttributes){
+        if (error) {
+            throw error;
+          }
+          var zip = new PizZip(content);
+          var doc = new Docxtemplater().loadZip(zip);
+          doc.setData(myAttributes);
+          try {
+            // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+            doc.render();
+          } catch (error) {
+            // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+            function replaceErrors(key, value) {
+              if (value instanceof Error) {
+                return Object.getOwnPropertyNames(value).reduce(function (
+                  error,
+                  key
+                ) {
+                  error[key] = value[key];
+                  return error;
+                },
+                {});
+              }
+              return value;
+            }
+            console.log(
+              JSON.stringify(
+                {
+                  error: error,
+                },
+                replaceErrors
+              )
+            );
+    
+            if (error.properties && error.properties.errors instanceof Array) {
+              const errorMessages = error.properties.errors
+                .map(function (error) {
+                  return error.properties.explanation;
+                })
+                .join("\n");
+              console.log("errorMessages", errorMessages);
+              // errorMessages is a humanly readable message looking like this :
+              // 'The tag beginning with "foobar" is unopened'
+            }
+            throw error;
+          }
+          var out = doc.getZip().generate({
+            type: "blob",
+            mimeType:
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+          }); //Output the document using Data-URI
+            //saveAs(out, "Proforma_"+myAttributes.numeroProforma+".docx"); //Descarga el archivo
+            let file = new File([out], "documento.docx", {type: ".docx", lastModified: Date.now()}) //lo convierte en un file
+             convertDocxToPDF(file,"Proforma"+myAttributes.numeroProforma+".pdf")
+             .then(()=>{this.descargando=false;
+              
+              this.descargarPDF.nativeElement.innerHTML='Descargar PDF';
+            });
+      }
+    /*Fin del script de word */
 }
